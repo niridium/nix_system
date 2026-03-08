54fe711f659d97079d93b7cdec474d7a6ccf7b9b
focusgraph <ruben.ledesma.go@protonmail.com>
2026-03-08 09:52:29 +0100

    niri config

--- machines/vega/home/niri.nix
+++ machines/vega/home/niri.nix
@@ -1,8 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs = {
     niri = {
-      package = pkgs.niri;
       settings = {
         spawn-at-startup = [
           {
@@ -11,30 +10,63 @@
             ];
           }
         ];
-        input.keyboard.xkb = {
-          layout = "us";
-          variant = "colemak";
+        input = {
+          keyboard.xkb = {
+            layout = "us";
+            variant = "colemak";
+          };
+          focus-follows-mouse.enable = true;
         };
         outputs = {
-          eDP-1.mode = {
-            height = 1200;
-            width = 1920;
-            refresh = 60.003;
+          eDP-1 = {
+            mode = {
+              height = 1200;
+              width = 1920;
+              refresh = 60.003;
+            };
           };
-          HDMI-A-1.mode = {
-            height = 1440;
-            width = 2560;
-            refresh = 143.999;
+          HDMI-A-1 = {
+            mode = {
+              height = 1440;
+              width = 2560;
+              refresh = 143.999;
+            };
+            position = {
+              x = -1536;
+              y = -240;
+            };
+            focus-at-startup = true;
           };
         };
-        # window-rules = {
-        #   geometry-corner-radius = 20;
-        #   clip-to-geometry = true;
-        # };
+        window-rules = [
+          {
+          clip-to-geometry = true;
+          geometry-corner-radius =
+          let
+            r = 10.;
+          in
+          {
+            top-left = r;
+            top-right = r;
+            bottom-left = r;
+            bottom-right = r;
+          };
+          }
+        ];
         layout = {
-          gaps = 16;
+          gaps = 8;
+          always-center-single-column = true;
+          default-column-width = { proportion = 1. / 2.; };
         };
+
+        prefer-no-csd = true;
         hotkey-overlay.skip-at-startup = true;
+
+        cursor = {
+          hide-after-inactive-ms = 4000;
+          hide-when-typing = true;
+        };
+
         binds =
         let
           spawnLocked = command: {
@@ -49,7 +81,7 @@
           "Mod+Space".action.spawn-sh = "noctalia-shell ipc call launcher toggle";
           "Mod+S".action.spawn-sh = "noctalia-shell ipc call controlCenter toggle";
           "Mod+Shift+G".action.spawn-sh = "noctalia-shell ipc call lockScreen lock";
-          "Mod+Q" = ( noRepeat // { action.close-window = []; });
+          "Mod+C" = ( noRepeat // { action.close-window = []; });
           "Mod+O" = ( noRepeat // { action.toggle-overview = []; });
           "Mod+H".action.focus-column-left = [];
           "Mod+J".action.focus-window-down = [];
