3a41c50a323e8bb6798296aef3939c79ed45fe0e
focusgraph <ruben.ledesma.go@protonmail.com>
2026-03-13 19:47:45 +0100

    cava config

---
+++ machines/vega/home/cava.nix
@@ -0,0 +1,28 @@
+{
+  programs.cava = {
+    enable = true;
+    settings = {
+      general = {
+        framerate = 120;
+        autosens = 1;
+        bars = 0; #auto
+        bar_width = 1;
+        bar_spacing = 1;
+        sleep_timer = 5;
+      };
+      input = {
+        method = "pipewire";
+        source = "auto";
+      };
+      output = {
+        method = "noncurses";
+        orientation = "horizontal";
+        channels = "stereo";
+        show_idle_bar_heads = 0;
+        waveform = 0;
+      };
+      color.theme = "noctalia";
+      smoothing.noise_reduction = 75;
+    };
+  };
+}
--- machines/vega/home/home.nix
+++ machines/vega/home/home.nix
@@ -6,6 +6,7 @@
     ../../../shared_home
     ./niri.nix
     ./yazi.nix
+    ./cava.nix
   ];
 
   home = {
