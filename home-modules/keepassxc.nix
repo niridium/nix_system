8c4537e491329e1f3464c8fae414071df28324dc
Focusgraph <ruben.ledesma.go@protonmail.com>
2026-03-18 08:39:40 +0100

    stop managing keepass config

--- home-modules/keepassxc.nix
+++ home-modules/keepassxc.nix
@@ -2,30 +2,32 @@
   programs.keepassxc = {
     enable = true;
     autostart = true;
-    settings = {
-      General = {
-        BackupBeforeSave = true;
-        BackupFilePathPattern = "/home/nixy/Documents/Keepass_Old/{DB_FILENAME}.old.kdbx";
-        ConfigVersion = 2;
-        SingleInstance = true;
-      };
-      GUI = {
-        ColorPasswords = true;
-        CompactMode = false;
-        MinimizeOnClose = true;
-        MinimizeOnStartup = true;
-        MinimizeToTray = true;
-        ShowTrayIcon = true;
-        TrayIconAppearance = "colorful";
-      };
-      Security = {
-        ClearClipboard = false;
-        LockDatabaseIdle = false;
-      };
-      Browser = {
-        Enabled = true;
-        UpdateBinaryPath = false;
-      };
-    };
+    # settings = {
+    #   General = {
+    #     BackupBeforeSave = true;
+    #     BackupFilePathPattern = "/home/nixy/Documents/Keepass_Old/{DB_FILENAME}.old.kdbx";
+    #     ConfigVersion = 2;
+    #     SingleInstance = true;
+    #   };
+    #   GUI = {
+    #     ColorPasswords = true;
+    #     CompactMode = false;
+    #     MinimizeOnClose = true;
+    #     MinimizeOnStartup = true;
+    #     MinimizeToTray = true;
+    #     ShowTrayIcon = true;
+    #     TrayIconAppearance = "colorful";
+    #   };
+    #   Security = {
+    #     ClearClipboard = false;
+    #     LockDatabaseIdle = false;
+    #   };
+    #   Browser = {
+    #     Enabled = true;
+    #     UpdateBinaryPath = false;
+    #     UseCustomBrowser = true;
+    #     CustomBrowserLocation = "~/.librewolf/native-messaging-hosts";
+    #   };
+    # };
   };
 }
