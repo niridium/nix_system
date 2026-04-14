457d0c2bef4942aedcc468293e5a15388485954a
niridium <niridium@tuta.io>
2026-04-14 09:16:06 +0200

    fix: add libraries to nix_ld

--- modules/core/nix_ld.nix
+++ modules/core/nix_ld.nix
@@ -4,6 +4,15 @@
     enable = true;
     libraries = with pkgs; [
       SDL2
+      libGL
+      libGLU
+      libx11
+      libxxf86vm
+      libxrandr
+      libxi
+      openal
+      glibc_multi
+      libcap
     ];
   };
 }
