52ede68ffdbf949abaddcccdc7b6e014a4574887
niridium <niridium@tuta.io>
2026-04-21 21:34:37 +0200

    fix: remove filesystems from licher hardware config

--- hosts/licher/hardware-configuration.nix
+++ hosts/licher/hardware-configuration.nix
@@ -8,28 +8,10 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/612a17e6-d14d-46d7-8395-44b34f494778";
-      fsType = "btrfs";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/BEF2-86A8";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  fileSystems."/storage" =
-    { device = "/dev/disk/by-uuid/155a6565-1240-4a8b-972c-5514331b149b";
-      fsType = "btrfs";
-    };
-
-  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
