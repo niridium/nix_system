58e8244faed58391bc4162e242a7eea409d606d2
niridium <niridium@tuta.io>
2026-04-26 13:55:02 +0200

    build(flake): 20260423 -> 20260426

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -5,23 +5,4 @@
     limine.enable = true;
     efi.canTouchEfiVariables = true;
   };
-
-  # Workaround for cryptoModules until the fix comes to 25.11
-  boot.initrd.luks.cryptoModules = [
-    "aes"
-    # "aes_generic"
-    "blowfish"
-    "twofish"
-    "serpent"
-    "cbc"
-    "xts"
-    "lrw"
-    "sha1"
-    "sha256"
-    "sha512"
-    "af_alg"
-    "algif_skcipher"
-    "cryptd"
-    "input_leds"
-  ];
 }
